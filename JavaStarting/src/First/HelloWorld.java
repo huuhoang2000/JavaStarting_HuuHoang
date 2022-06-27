77dea0b90e3ee7f9deacba24d45605e3d3843232
huuhoang2000 <chinhhuu2000@gmail.com>
2022-06-26 20:11:07 -0600

    Check Commit for Java Starting

--- JavaStarting/src/First/HelloWorld.java
+++ JavaStarting/src/First/HelloWorld.java
@@ -1,7 +1,28 @@
 package First;
 
+import java.sql.Date;
+import java.util.Arrays;
+
 public class HelloWorld {
 	public static void main (String [] args) {
-		System.out.println("Hello World");
+//		System.out.println("Hello World");
+//		int age = 30;
+		//primitive data type, JRE will allocate and release
+//		Date now = new Date(age);
+		//reference data type, allocate memory for now variable, JRE will release
+		//object now is an instance of class Date
+		
+		int[] numbers1 = new int[5];
+		numbers1[0] = 1;
+		numbers1[1] = 1;
+		numbers1[2] = 1;
+		numbers1[3] = 1;
+		numbers1[4] = 1;
+//		System.out.println(numbers);
+		System.out.println(Arrays.toString(numbers1));
+		
+		int[] numbers2 = {1, 6, 3, 5, 8};
+		Arrays.sort(numbers2);
+		System.out.println(Arrays.toString(numbers2));
 	}
 }
